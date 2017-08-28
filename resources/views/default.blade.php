4f69821bed5624148d48cf09ecde8b8cf6a855da
Inder <isahuja1@yahoo.com>
2017-08-28 22:40:31 +0530

    dwwf

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -6,7 +6,7 @@
 <body id="page-top">
 	<nav class="navbar navbar-expand-lg navbar-light " id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="/">PC Help</a>
+        <a class="navbar-brand js-scroll-trigger" href="/">PC Helpline</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
